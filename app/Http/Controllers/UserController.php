5346ccec31ccd6c6d5a796ec8586b2e07e676308
Davila99 <davilaeliseo453@gmail.com>
2021-11-18 17:25:58 -0600

    Modificacion de los metodos necesarios para mostrar e eliminar datos la tabla user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
+
 class UserController extends Controller
 {
+
     public function index()
     {
         $datos['users'] = User::paginate(5);
+
         return view('user/index', $datos);
     }
     public function destroy($id)
