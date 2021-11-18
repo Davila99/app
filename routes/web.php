af85a660c0be494318cf956825fdfd762b545fcd
Davila99 <davilaeliseo453@gmail.com>
2021-11-18 17:30:34 -0600

    configuracion de la ruta para anceder al al controllador UserController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,13 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middeleware' => 'auth'], function () {
+    Route::resource('user', UserController::class)->middleware('auth');
+    Route::get('/home', [App\Http\Controllers\UserController::class, 'index'])->name('home')->middleware('auth');
+});
